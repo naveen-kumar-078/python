d9d0e733ad335bac29ff8d139ce6589dd270cece
naveen <naveenkumar071224@gmail.com>
2026-03-18 18:20:29 +0530

    python

--- input.py
+++ input.py
@@ -15,30 +15,59 @@
 
 
 
-# import math
+
 # a = int(input())
 # b = int(input())
 # c = int(input())
 # d = a*b*c
 # e = a+b+c
 # f = d/e
-# print(math.de(f))
+# print((f))
 
 
 
 
 
-name  = input()
-mark = int(input())
-departmnet = input()
+# name  = input()
+# mark = int(input())
+# departmnet = input()
 
-print("my name is ",name)
-print("my score is",mark/10,"/10")
-print("my department is ", departmnet)
-
-
+# print("my name is ",name)
+# print("my score is",mark/10,"/10")
+# print("my department is ", departmnet)
 
 
 
 
 
+
+
+
+# x,y,z = "orange","beetroot","pinapple"
+# print(x)
+# print(y)
+# print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = "hi"
+
+
+def mufunc():
+    global x
+    x = "fanstaic"
+
+mufunc()
+
+print("hello",x)
