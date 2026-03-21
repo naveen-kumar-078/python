13d7ab523efb3ec8f8b83ab1e9742c35471f97ea
naveen <naveenkumar071224@gmail.com>
2026-03-21 12:48:45 +0530

    python

--- pratising problem ,/string.py
+++ pratising problem ,/string.py
@@ -1,2 +1,22 @@
-a = "Hello World"
-print("upper case",)
+
+# a = input()
+
+# print("uppercase",a.upper())
+# print("lowercase:",a.lower())
+# print("length",len(a))
+
+
+
+# b = "python"
+# print(b[3:])
+
+s = "hello world"
+print(s[:5])
+c ="hello world"
+print(s[5:])
+
+revrse = "hello world"
+print(s[::-1])
+
+
+
