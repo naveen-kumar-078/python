5e376242be6c963d16dbf4797bf5468f91649251
naveen <naveenkumar071224@gmail.com>
2026-08-17 21:38:24 +0530

    py the list

---
+++ pylists/pylists.py
@@ -0,0 +1,31 @@
+#list can be changed ,ordered and be duplicate
+
+#normal list
+
+lst =  [1,2,3]
+print(lst)
+
+# lists can be duplicate
+
+thislisys = ["apple","orange","mango","orange"]
+print(thislisys)
+
+#list length
+
+print(len(thislisys))
+
+
+# use can use list in any datatypes
+
+list1 = [0,1,2,3]
+list2 = ["apple","orange","banan"]
+list3 = [True,False,True]
+print(list1,list2,list3)
+
+
+
+
+#using list() constructor
+
+thelis = list(("apple","orange","banana"))
+print(thelis)
--- string/modifystr.py
+++ string/modifystr.py
@@ -16,3 +16,10 @@ print(c.replace("o","d"))
 #remove white space
 d= " hello world "
 print(d.strip()) 
+
+
+
+e =  "hello word"
+revrse = "".join(reversed(e))
+print(revrse)
+
