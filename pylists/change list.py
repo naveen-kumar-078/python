6398e4bdc3135a58db5c5c11c63b3f34e5bec10b
naveen <naveenkumar071224@gmail.com>
2026-08-19 22:03:22 +0530

    py list

---
+++ pylists/change list.py
@@ -0,0 +1,31 @@
+ # change item of the lisrt
+thelist = ["apple","orange","banana"]
+thelist[2]="blackcurrent"
+print(thelist)
+
+# chnage the list using the ramge 
+
+thelist1 = ["apple","orange","mango","banna","watermelon"]
+thelist1[1:3]=["carrot","beetroot"]
+print(thelist1)
+
+# adding two vales by removing one value
+
+thelist1 = ["apple","orange","mango"]
+thelist1[1:3]=["carrot","beetroot"]
+print(thelist1)
+
+
+
+#If you insert more items than you replace
+
+num1 = [10,20,30,40,52]
+num1[1:2]=[75,85]
+print(num1)
+
+
+#If you insert more items than you replace
+
+num1 = [10,20,30,40,52]
+num1[1:4]=[75]
+print(num1)
