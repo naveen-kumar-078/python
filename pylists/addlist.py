129655d69e8ae99ffda238ce7490e2ec3484e8f4
naveen <naveenkumar071224@gmail.com>
2026-08-24 23:00:54 +0530

    py list

--- pylists/addlist.py
+++ pylists/addlist.py
@@ -1,20 +1,54 @@
-#o add an item to the end of the list, use the append() method:
+# #o add an item to the end of the list, use the append() method:
 
-thislisys = ["apple","orange","mango","orange"]
-thislisys.append("pineapple")
-print(thislisys)
-
-
-# insert an item
-
-thislisys = ["apple","orange","mango","orange"]
-thislisys.insert(3, "pinabble")
-print(thislisys)
+# thislisys = ["apple","orange","mango","orange"]
+# thislisys.append("pineapple")
+# print(thislisys)
 
 
-# to add elemnsts from another list to current list
+# # insert an item
 
-thislisys = ["apple","orange","mango","orange"]
-veg = ["carrot","beetrooot","totmato"]
-thislisys.extend(veg)
-print(thislisys)
+# thislisys = ["apple","orange","mango","orange"]
+# thislisys.insert(3, "pinabble")
+# print(thislisys)
+
+
+# # to add elemnsts from another list to current list
+
+# thislisys = ["apple","orange","mango","orange"]
+# veg = ["carrot","beetrooot","totmato"]
+# thislisys.extend(veg)
+# print(thislisys)
+
+
+
+
+
+
+fruits = ["apple","orage","mago"]
+fruits.append("bannna")
+print(fruits)
+
+
+
+
+fruits = ["apple","orage","mago"]
+fruits.insert(2,"bannna")
+print(fruits)
+
+
+fruits = ["apple","orage","mago"]
+veg = ["carrot","beetroot","tomato"]
+fruits.extend(veg)
+print(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pylists/remove.py
@@ -0,0 +1,38 @@
+# to remove spcify item
+
+thelist = ["apple","banna","orane"]
+thelist.remove("banna")
+print(thelist)
+
+
+# if there is an more item with the specified item alos it removes
+
+thelist = ["apple","banna","orane","banna"]
+thelist.remove("banna")
+print(thelist)
+
+
+
+#using index valeus we can remove using pop
+
+thelist = ["apple","banna","orane"]
+thelist.pop(1)
+print(thelist)
+
+# del functin also remove the specify utem
+thelist = ["apple","banna","orane"]
+del thelist
+
+
+
+
+
+#clear list
+
+thelist = ["apple","banna","orane"]
+thelist.clear()
+print(thelist)
+
+
+
+
