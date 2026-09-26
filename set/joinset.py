d25c237fef608ed944d038fac117949c58903d72
naveen <naveenkumar071224@gmail.com>
2026-09-26 22:11:51 +0530

    python set

--- set/joinset.py
+++ set/joinset.py
@@ -34,3 +34,65 @@ y = (1, 2, 3)
 
 z = x.union(y)
 print(z)
+
+
+
+
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+setc = set1.union(set2)
+
+
+set8 = {"apple","orange","mango"}
+se9 = {1,2,3}
+set10 = {"jhon","elena"}
+
+allset = set8.union(se9,set10)
+print(allset)
+
+
+
+
+aset = {1,2,3}
+btuple = (4,5,6)
+all = aset.union(btuple)
+print(all)
+
+print("----------intersection")
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+c = set1.intersection(set2)
+print(c)
+
+a = {0,1,"apple","orange"}
+b = {True,False,"banna","pista"}
+c = a.intersection(b)
+print(c)
+
+print("--------------------------setjoindiffrenece------------------")
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.difference(set2)
+print(set3)
+
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.difference_update(set2)
+print(set3)
+
+
+
+print("-------------------symatic differnece---------------")
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.symmetric_difference(set2)
+print(set3)
+
+
